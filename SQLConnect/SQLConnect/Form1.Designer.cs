ed0ee23050b465a0e6b2c1caff99eb61636fbf65
haenghaeng <ymca0147@naver.com>
2019-01-29 23:56:33 +0900

    Add files via upload

---
+++ SQLConnect/SQLConnect/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace SQLConnect
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCreateTable = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCreateTable
+            // 
+            this.btnCreateTable.Location = new System.Drawing.Point(37, 12);
+            this.btnCreateTable.Name = "btnCreateTable";
+            this.btnCreateTable.Size = new System.Drawing.Size(199, 61);
+            this.btnCreateTable.TabIndex = 0;
+            this.btnCreateTable.Text = "DB생성";
+            this.btnCreateTable.UseVisualStyleBackColor = true;
+            this.btnCreateTable.Click += new System.EventHandler(this.btnCreateTable_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnCreateTable);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCreateTable;
+    }
+}
+
---
+++ SQLConnect/SQLConnect/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using MySql;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace SQLConnect
+{
+    public partial class Form1 : Form
+    {
+        private MySqlConnection Connection;
+        private MySqlCommand Command = new MySqlCommand();
+        private MySqlDataReader DataReader;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void CreateTable()
+        {
+            if (Connection.State == ConnectionState.Closed)
+            {
+                Connection.Open();
+            }
+
+            try
+            {
+                Command = new MySqlCommand("CREATE TABLE IF NOT EXISTS TEST_TABLE (" +
+                                        "IDX INT NOT NULL primary key AUTO_INCREMENT, " +
+                                        "A CHAR(1) NOT NULL , " +
+                                        "B CHAR(1) NOT NULL )", Connection);
+
+                Command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("FAILED CREATE TABLE : " + ex.Message);
+            }
+            Connection.Close();
+        }
+
+        public void TEST_InsertDB()
+        {
+            if (Connection.State == ConnectionState.Closed)
+            {
+                Connection.Open();
+            }
+
+            try
+            {
+                Command = new MySqlCommand("INSERT INTO TEST_TABLE" + "(A, B)" + "VALUES (@A, @B)", Connection);
+                Command.Parameters.AddWithValue("@A", "A");
+                Command.Parameters.AddWithValue("@B", "B");
+                Command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+            }
+            Connection.Close();
+        }
+
+        private void CreateDatabase()
+        {
+            Connection.Open();
+            string cmd = "CREATE DATABASE IF NOT EXISTS `TEST`;";
+            Command = new MySqlCommand(cmd, Connection);
+            Command.ExecuteNonQuery();
+            Connection.Close();
+        }
+
+        public void ConnectDB()
+        {
+            Connection = null;
+            try
+            {
+                string connectionString = "Data Source='IP'; port='PORT'; Initial Catalog=TEST; user id='ID'; password='PASSWORD'; protocol=tcp; Character Set=utf8; Connection timeout=15; Connection lifetime=0; Max Pool Size=100; Min Pool Size=10; allow user variables=true";
+                Connection = new MySqlConnection(connectionString);
+            }
+            catch (Exception ex)
+            {
+            }
+            finally
+            {
+            }
+        }
+
+        private void btnCreateTable_Click(object sender, EventArgs e)
+        {
+            ConnectDB();
+            CreateDatabase();
+            CreateTable();
+            TEST_InsertDB();
+        }
+    }
+}
